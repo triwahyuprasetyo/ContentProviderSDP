b87bdd81a33bd215ba91168d5dfbbe59219a697f
why <triwahyuprasetyo@gmail.com>
2016-07-12 12:37:47 +0700

    first update from local

---
+++ app/src/main/java/com/why/contentprovidersdp/MainActivity.java
@@ -0,0 +1,113 @@
+package com.why.contentprovidersdp;
+
+import android.content.ContentValues;
+import android.database.Cursor;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private Button buttonAddName, buttonRetrieveStudent;
+    private EditText editTextName, editTextGrade;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+        buttonAddName = (Button) findViewById(R.id.buttonAddName);
+        buttonAddName.setOnClickListener(this);
+        buttonRetrieveStudent = (Button) findViewById(R.id.buttonRetrieveStudent);
+        buttonRetrieveStudent.setOnClickListener(this);
+
+        editTextName = (EditText) findViewById(R.id.editTextName);
+        editTextGrade = (EditText) findViewById(R.id.editTextGrade);
+    }
+
+    public void onClickAddName(View view) {
+        // Add a new student record
+        ContentValues values = new ContentValues();
+
+        values.put(StudentsProvider.NAME,
+                ((EditText) findViewById(R.id.editTextName)).getText().toString());
+
+        values.put(StudentsProvider.GRADE,
+                ((EditText) findViewById(R.id.editTextGrade)).getText().toString());
+
+        Uri uri = getContentResolver().insert(
+                StudentsProvider.CONTENT_URI, values);
+
+        Toast.makeText(getBaseContext(),
+                uri.toString(), Toast.LENGTH_LONG).show();
+    }
+
+    public void onClickRetrieveStudents(View view) {
+
+        // Retrieve student records
+        String URL = "content://com.why.contentprovidersdp.College/students";
+
+        Uri students = Uri.parse(URL);
+        Cursor c = managedQuery(students, null, null, null, "name");
+
+        if (c.moveToFirst()) {
+            do {
+                Toast.makeText(this,
+                        c.getString(c.getColumnIndex(StudentsProvider._ID)) +
+                                ", " + c.getString(c.getColumnIndex(StudentsProvider.NAME)) +
+                                ", " + c.getString(c.getColumnIndex(StudentsProvider.GRADE)),
+                        Toast.LENGTH_SHORT).show();
+            } while (c.moveToNext());
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onClick(View view) {
+        if (view.getId() == buttonAddName.getId()) {
+            onClickAddName(view);
+        } else if (view.getId() == buttonRetrieveStudent.getId()) {
+            onClickRetrieveStudents(view);
+        }
+    }
+}
